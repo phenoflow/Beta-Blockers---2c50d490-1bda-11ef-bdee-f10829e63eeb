6306fc1941bc49358f6647eef80ac6a0769c5093
Martin Chapman <contact@martinchapman.co.uk>
2024-05-27 04:39:21 +0100

    Update made by Phenoflow

---
+++ python/beta-blockers-3125mg---primary.py
@@ -0,0 +1,25 @@
+# Victor W Zhong, Juhaeri Juhaeri, Stephen R Cole, Christian M Shay, Carolyn A Chew-Graham, Penny Gordon-Larsen, Evangelos Kontopantelis, Elizabeth J Mayer-Davis, 2024.
+
+import sys, csv, re
+
+codes = [{"code":"2629","system":"gprdproduct"},{"code":"32094","system":"gprdproduct"},{"code":"51528","system":"gprdproduct"},{"code":"8189","system":"gprdproduct"},{"code":"53885","system":"gprdproduct"},{"code":"817","system":"gprdproduct"},{"code":"34449","system":"gprdproduct"},{"code":"58511","system":"gprdproduct"},{"code":"7066","system":"gprdproduct"},{"code":"60761","system":"gprdproduct"},{"code":"33374","system":"gprdproduct"},{"code":"46935","system":"gprdproduct"},{"code":"59495","system":"gprdproduct"},{"code":"9783","system":"gprdproduct"},{"code":"52548","system":"gprdproduct"},{"code":"34501","system":"gprdproduct"},{"code":"31708","system":"gprdproduct"},{"code":"61663","system":"gprdproduct"},{"code":"46936","system":"gprdproduct"},{"code":"43251","system":"gprdproduct"},{"code":"54479","system":"gprdproduct"},{"code":"8061","system":"gprdproduct"},{"code":"50403","system":"gprdproduct"},{"code":"34034","system":"gprdproduct"},{"code":"1288","system":"gprdproduct"},{"code":"599","system":"gprdproduct"},{"code":"34825","system":"gprdproduct"},{"code":"34741","system":"gprdproduct"}];
+REQUIRED_CODES = 1;
+with open(sys.argv[1], 'r') as file_in, open('beta-blockers-potential-cases.csv', 'w', newline='') as file_out:
+    csv_reader = csv.DictReader(file_in)
+    csv_writer = csv.DictWriter(file_out, csv_reader.fieldnames + ["beta-blockers-3125mg---primary-identified"])
+    csv_writer.writeheader();
+    codes_identified = 0;
+    for row in csv_reader:
+        newRow = row.copy();
+        for cell in row:
+            # Iterate cell lists (e.g. codes)
+            for item in re.findall(r'\(([^,]*)\,', row[cell]):
+                if(item in list(map(lambda code: code['code'], codes))): codes_identified+=1;
+                if(codes_identified>=REQUIRED_CODES):
+                    newRow["beta-blockers-3125mg---primary-identified"] = "CASE";
+                    break;
+            if(codes_identified>=REQUIRED_CODES): break;
+        if(codes_identified<REQUIRED_CODES):
+            newRow["beta-blockers-3125mg---primary-identified"] = "UNK";
+        codes_identified=0;
+        csv_writer.writerow(newRow)
